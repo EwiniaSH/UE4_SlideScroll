b3921c4f6a90848972a4ee9af6a300414675922a
EwiniaSH <112123@naver.com>
2022-10-08 14:43:11 +0900

    프로젝트 생성

---
+++ Source/SlideScroll/SlideScrollGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlideScrollGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLIDESCROLL_API ASlideScrollGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
