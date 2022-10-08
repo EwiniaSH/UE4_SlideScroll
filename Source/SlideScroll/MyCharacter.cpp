3316dee1356ffb4468dff9420dde02b0d7393475
EwiniaSH <112123@naver.com>
2022-10-09 00:57:35 +0900

    캐릭터 리소스 및 기본 움직임 추가

---
+++ Source/SlideScroll/MyCharacter.cpp
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Animation/AnimInstance.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerController.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 800.0f;
+	SpringArm->bInheritYaw = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpringArm->SetRelativeRotation(FRotator(-15.0f, 0.0f, 0.0f));
+	AnimInstance = Cast<UAnimInstance>(GetMesh()->GetAnimInstance());
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("XAxis", this, &AMyCharacter::Movement);
+	PlayerInputComponent->BindAction("Attack", IE_Pressed, this, &AMyCharacter::Attack);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMyCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AMyCharacter::StopJumping);
+}
+
+void AMyCharacter::Movement(float Value)
+{
+	if (!AnimInstance->Montage_IsPlaying(Attack_Anim))
+	{
+		AddMovementInput(FVector(0.0f, 1.0f, 0.0f), Value * 20.0f);
+	}
+}
+
+void AMyCharacter::Attack()
+{
+	if (!AnimInstance->Montage_IsPlaying(Attack_Anim))
+	{
+		PlayAnimMontage(Attack_Anim);
+	}
+}
+
+void AMyCharacter::Jump()
+{
+	Super::Jump();
+
+	if (!AnimInstance->Montage_IsPlaying(Jump_Anim))
+	{
+		PlayAnimMontage(Jump_Anim);
+	}
+}
+
+void AMyCharacter::Landed(const FHitResult& Hit)
+{
+	Super::Landed(Hit);
+
+	if (AnimInstance->Montage_IsPlaying(Jump_Anim))
+	{
+		StopAnimMontage(Jump_Anim);
+	}
+}
---
+++ Source/SlideScroll/MyCharacter.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class SLIDESCROLL_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void Movement(float Value);
+	void Attack();
+	virtual void Jump() override;
+	virtual void Landed(const FHitResult& Hit) override;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	USpringArmComponent* SpringArm;
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	UCameraComponent* Camera;
+	UPROPERTY(EditAnywhere, Category = Animation)
+	UAnimMontage* Attack_Anim;
+	UPROPERTY(EditAnywhere, Category = Animation)
+	UAnimMontage* Jump_Anim;
+
+	UAnimInstance* AnimInstance;
+};
